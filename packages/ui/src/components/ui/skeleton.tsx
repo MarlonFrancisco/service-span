315e79ac6b34f94aa8aebcf31bb918dbe905381b
Marlon Francisco <marlon.nascimento@riachuelo.com.br>
2025-11-10 00:47:37 +0000

    style(ui): enhance skeleton component animations

--- packages/ui/src/components/ui/skeleton.tsx
+++ packages/ui/src/components/ui/skeleton.tsx
@@ -1,13 +1,16 @@
-import { cn } from "@repo/ui/lib/utils"
+import { cn } from '@repo/ui/lib/utils';
 
-function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
+function Skeleton({ className, ...props }: React.ComponentProps<'div'>) {
   return (
     <div
       data-slot="skeleton"
-      className={cn("bg-neutral-100 animate-pulse rounded-md dark:bg-neutral-800", className)}
+      className={cn(
+        'bg-gray-200 animate-pulse rounded-md dark:bg-gray-800',
+        className,
+      )}
       {...props}
     />
-  )
+  );
 }
 
-export { Skeleton }
+export { Skeleton };
