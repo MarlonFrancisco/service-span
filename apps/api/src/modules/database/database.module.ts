c670506eb8b94ccbfdcf1d28ff60ad749a904f64
Marlon Francisco <marlon.nascimento@riachuelo.com.br>
2025-11-22 19:57:14 +0000

    chore(api): use db url on db connection

--- apps/api/src/modules/database/database.module.ts
+++ apps/api/src/modules/database/database.module.ts
@@ -13,11 +13,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
         return {
           type: 'postgres',
-          host: configService.get<string>('DB_HOST', 'localhost'),
-          port: configService.get<number>('DB_PORT', 5432),
-          username: configService.get<string>('DB_USERNAME', 'postgres'),
-          password: configService.get<string>('DB_PASSWORD', 'postgres'),
-          database: configService.get<string>('DB_DATABASE', 'service_snap'),
+          url: configService.get<string>('DB_URL'),
           entities: [__dirname + '/../**/*.entity{.ts,.js}'],
           synchronize: configService.get<boolean>('DB_SYNCHRONIZE', false),
           logging: configService.get<boolean>('DB_LOGGING', false),
