554b22bd04323ebf06fa62fa1ff24d1ce3675385
Marlon Francisco <marlon.nascimento@riachuelo.com.br>
2025-12-09 01:03:59 +0000

    feat(api/search): add store search index builder
    Implement mountStoreSearchIndex function to prepare store data for search indexing

--- apps/api/src/utils/helpers/search.helpers.ts
+++ apps/api/src/utils/helpers/search.helpers.ts
@@ -1,7 +1,48 @@
+import type {
+  IStoreSearchContent,
+  IStoreSearchMetadata,
+} from 'src/modules/search/search.types';
 import type { Store } from '../../modules/partner/stores/store.entity';
 
 export const availableToIndexStore = (store: Store): boolean => {
   return (
     store.isActive && store.gallery.length > 0 && store.storeMembers.length > 0
   );
+};
+
+export const mountStoreSearchIndex = (
+  store: Store,
+): {
+  content: IStoreSearchContent;
+  metadata: IStoreSearchMetadata;
+} => {
+  return {
+    content: {
+      name: store.name,
+      description: store.description,
+      country: store.country,
+      address: store.address,
+      city: store.city,
+      state: store.state,
+      zipCode: store.zipCode,
+      services: store.services,
+      categories: store.categories,
+      openTime: store.openTime,
+      closeTime: store.closeTime,
+      businessDays: store.businessDays,
+    },
+    metadata: {
+      ownerId: store.owner.id,
+      createdAt: store.createdAt.toISOString(),
+      telephone: store.telephone,
+      email: store.email,
+      website: store.website,
+      instagram: store.instagram,
+      facebook: store.facebook,
+      amenities: store.amenities,
+      gallery: store.gallery,
+      reviews: store.reviews,
+      currency: store.currency,
+    },
+  };
 };
